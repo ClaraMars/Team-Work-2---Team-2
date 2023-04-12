c0b53731eaba17ff35d52b68e5dff30fd3f8ee16
fehdk <fahdkhouttaif@gmail.com>
2023-04-12 14:22:51 +0200

    TODO : Bloque2 ejercicio8

--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -3,3 +3,14 @@ const randomNumbers = (max, min) => {
     return random;
 };   
 
+//Bloque2, ejercicio3
+// Bucle for para imprimir 4 filas
+for (let i = 0; i < 4; i++) {
+    // Bucle for para imprimir 4 asteriscos en cada fila
+    for (let j = 0; j < 4; j++) {
+      // Imprime un asterisco sin salto de línea
+      process.stdout.write('*');
+    }
+    // Imprime un salto de línea después de cada fila
+    console.log();
+  }
