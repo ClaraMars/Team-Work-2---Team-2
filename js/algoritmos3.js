b9259d1612661328e28df80002e762c2734caaee
gisellecole <giselle_cole@icloud.com>
2023-04-12 15:15:59 +0200

    some improvement added if statement to check if  is a number

--- js/algoritmos3.js
+++ js/algoritmos3.js
@@ -8,41 +8,43 @@ dentro del array, con el siguiente formato (por consola):
 • min value: nnn
 • max value: mmm */
 const a3e1 = () => {
-
   // function to add a leading 0 for positive and negative numbers
-
-function pad(num, size) {
-
-    /*This if statement to check if a negative number is provided to the function. If it is, it removes the minus sign from the number, then uses the padStart method to add leading zeros to the positive absolute value, and re-adds the minus sign. If the number is positive, it simply uses the padStart method to add leading zeros to the number. */
-    if (num < 0) {// check if the number is negative
-      const withoutMinus = String(num).slice(1);// remove the minus sign from the number
-      return '-' + withoutMinus.padStart(size, '0');// add leading zeros to the positive absolute value and re-add the minus sign
+  function pad(num, size) {
+    /*This if statement to check if a negative number is provided to the function. If it is, 
+    it removes the minus sign from the number, then uses the padStart method to add leading zeros 
+    to the positive absolute value, and re-adds the minus sign. If the number is positive, it simply 
+    uses the padStart method to add leading zeros to the number. */
+    if (num < 0) {
+      // check if the number is negative
+      const withoutMinus = String(num).slice(1); // remove the minus sign from the number
+      return "-" + withoutMinus.padStart(size, "0"); // add leading zeros to the positive absolute value and re-add the minus sign
     }
-    return String(num).padStart(size, '0');// add leading zeros to the number
+    return String(num).padStart(size, "0"); // add leading zeros to the number
   }
-//   console.log(pad(3, 3));
-//   console.log(pad(-3, 3));
-  
-
   // User input maximum and minimum value
-  let min = parseInt(prompt("Enter the  minimum value:"));
-  let max = parseInt(prompt("Enter the maximum value:"));
-
+  // added isNan to check if the user enters a non-numeric value.
+  let min, max;
+  while (true) {
+    min = prompt("Enter the minimum value:");
+    max = prompt("Enter the maximum value:");
+  
+    if (!isNaN(min) && !isNaN(max)) { // check if both values are numbers
+      min = parseInt(min);
+      max = parseInt(max);
+      break;
+    } else {
+      alert("Both values must be numbers. Please try again.");
+    }
+  }
   // Generate tweenty integer random numbers
   const array = Array.from({ length: 20 }, () =>
     Math.floor(Math.random() * (max - min + 1) + min)
   );
-
   // Include the given minimum and maximum values in the array
   array[0] = min;
   array[19] = max;
   console.log(array);
-
-  // Read the highest number and the smaller number inside the array using (...) => spread operator.
-//   const minValue = Math.min(...array);
-//   const maxValue = Math.max(...array);
-
-// add leading zeros to the number
+  // add leading zeros to the number
   let resultMin = pad(min, 3);
   let resultMax = pad(max, 3);
 
@@ -56,21 +58,22 @@ function pad(num, size) {
 // --------------------------------------------------------
 
 /* 7. Ejercicio
-Escribe un programa que genera 100 números aleatorios, entre 0 y 500, y los almacena en un array. A continuación filtra todos los números impares, ordenando los pares de mayor a menor.
+Escribe un programa que genera 100 números aleatorios, entre 0 y 500, y los almacena en un array. 
+A continuación filtra todos los números impares, ordenando los pares de mayor a menor.
 */
 const a3e7 = () => {
-let min = 0;
-let max = 500;
- // Generate tweenty integer random numbers
- const array = Array.from({ length: 100 }, () =>
- Math.floor(Math.random() * (max - min + 1) + min)
-);
-console.log(array);
+  let min = 0;
+  let max = 500;
+  // Generate tweenty integer random numbers
+  const array = Array.from({ length: 100 }, () =>
+    Math.floor(Math.random() * (max - min + 1) + min)
+  );
+  console.log(array);
 
-// Filter even numbers and sort in descending order
-let pair = array.filter(n => n%2 === 0)
-let pairSorted = pair.sort((a, b) => b - a);
-console.log(pairSorted);
+  // Filter even numbers and sort in descending order
+  let pair = array.filter((n) => n % 2 === 0);
+  let pairSorted = pair.sort((a, b) => b - a);
+  console.log(pairSorted);
 };
 // call the arrow function a3e7
-a3e7();
+// a3e7();
