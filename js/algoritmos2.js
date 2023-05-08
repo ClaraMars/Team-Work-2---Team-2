bc26045e47413bb5c2d64917be74ddb2ffc3dccb
Clara Márquez <clara.marquez.torres@gmail.com>
2023-05-08 13:58:31 +0200

    Corrección algoritmos 2 08.05

--- js/algoritmos2.js
+++ js/algoritmos2.js
@@ -6,20 +6,37 @@ const randomNumbers = (max, min) => {
 //EJERCICIO 1
 //Escribe un programa para contar las letras en la palabra: palindrome, mostrándolas una a una.
 
+// const a2e1 = () => {
+
+//     let array = "palindrome";
+
+//     console.log(`La palabra ${array} tiene un total de ${array.length} letras:`);
+//      [...text].forEach((letter) => console.log(letter));
+//      Array.from(text).forEach((letter) => console.log(letter));
+//      text.split("").forEach((letter) => console.log(letter));
+//      text.split("").map((letter) => console.log(letter));
+
+//         for (let letter = 0; letter < totalLetters; letter++) {
+//             let singleLetter = array[index].charAt(letter);
+//             console.log(singleLetter);
+//         };
+//     };
+// };
+
 const a2e1 = () => {
 
-    let array = ["palindrome"];
+  let array = ["palindrome"];
 
-    for (let index = 0; index < array.length; index++) {
+  for (let index = 0; index < array.length; index++) {
 
-        let totalLetters = array[index].length;
-        console.log(`La palabra ${array[index]} tiene un total de ${totalLetters} letras:`);
+      let totalLetters = array[index].length;
+      console.log(`La palabra ${array[index]} tiene un total de ${totalLetters} letras:`);
 
-        for (let letter = 0; letter < totalLetters; letter++) {
-            let singleLetter = array[index].charAt(letter);
-            console.log(singleLetter);
-        };
-    };
+      for (let letter = 0; letter < totalLetters; letter++) {
+          let singleLetter = array[index].charAt(letter);
+          console.log(singleLetter);
+      };
+  };
 };
 
 //a2e1();
@@ -29,13 +46,24 @@ const a2e1 = () => {
 
 const a2e2 = () => {
   function a2e2(cad) {
-    let separarCadena = cad.split("");
-    let invertirArray = separarCadena.reverse();
-    let unirArray = invertirArray.join("");
-    return unirArray;
+    let separarCadena = cad.split("").reverse().join("");
+    // let invertirArray = separarCadena.reverse();
+    // let unirArray = invertirArray.join("");
+    
+    // let separarCadena = cad.split("");
+    // let invertirArray = separarCadena.reverse();
+    // let unirArray = invertirArray.join("");
+    return separarCadena;
   }
   
   console.log(a2e2("no lemon, no melon"));
+
+  // let palindrome = "no lemon, no melon";
+  // let invertedPalindrome = "";
+  // for (let start = palindrome.length - 1; start >= 0; start --) {
+  //   invertedPalindrome += palindrome[start];
+  // }
+  // console.log(invertedPalindrome);
 };
 
 //a2e2();
@@ -46,8 +74,8 @@ const a2e2 = () => {
 const a2e3 = () => {
   
   let fila = ""; // Inicializa una cadena vacía para construir la fila
-  for (let i = 0; i < 4; i++) { // Bucle for para imprimir 4 filas
-    for (let j = 0; j < 4; j++) { // Bucle for para imprimir 4 asteriscos en cada fila  
+  for (let row = 0; row < 4; row++) { // Bucle for para imprimir 4 filas
+    for (let character = 0; character < 4; character++) { // Bucle for para imprimir 4 asteriscos en cada fila  
       fila += "*"; // Concatena un asterisco a la cadena de la fila
     };
     fila += "\n";
@@ -61,16 +89,24 @@ const a2e3 = () => {
 //Escribe un programa con 7 variables con la temperatura de los 7 últimos días (Google). A continuación, muestra cuál ha sido la temperatura media.
 
 const a2e4 = () => {
-    let temp1 = parseInt("25");
-    let temp2 = parseInt("27");
-    let temp3 = parseInt("24");
-    let temp4 = parseInt("26");
-    let temp5 = parseInt("28");
-    let temp6 = parseInt("25");
-    let temp7 = parseInt("24");
+    // let temp1 = parseInt("25");
+    // let temp2 = parseInt("27");
+    // let temp3 = parseInt("24");
+    // let temp4 = parseInt("26");
+    // let temp5 = parseInt("28");
+    // let temp6 = parseInt("25");
+    // let temp7 = parseInt("24");
 
-    let tempMedia = ((temp1 + temp2 + temp3 + temp4 + temp5 + temp6 + temp7)/7).toFixed(2);
-    console.log(`La temperatura media de los últimos 7 días es: ${tempMedia}°C.`);
+    // let tempMedia = ((temp1 + temp2 + temp3 + temp4 + temp5 + temp6 + temp7)/7).toFixed(2);
+    // console.log(`La temperatura media de los últimos 7 días es: ${tempMedia}°C.`);
+
+    let temp = [25, 27, 24, 26, 28, 25, 24];
+    let length = temp.length;
+    let sum = 0;
+    for (let index = 0; index < length; index++) {
+        sum += temp[index];
+    }
+    console.log(`La temperatura media de los últimos 7 días es: ${(sum/length).toFixed(2)}°C.`);
 }
 
 //a2e4();
@@ -231,7 +267,7 @@ const a2e5 = () => {
   stringManager();
 };
 
-// a2e5();
+a2e5();
 
 //EJERCICIO 6
 //Escribe un programa para generar estadísticas de texto. Recibirá un string proporcionado por el usuario, y a modo de ejemplo, para la entrada: Lorem ipsum dolor sit amet, mostrará la siguiente salida:
